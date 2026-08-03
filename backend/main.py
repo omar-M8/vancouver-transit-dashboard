ba8399fa5d7643b12d5baa76ff8c8d2db1c8128c
Omar <mohammed.omar078@gmail.com>
2026-08-03 16:04:27 -0700

    Implement dynamic transit endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/transit/{station_id}")
+async def get_transit_data(station_id: str):
+    return {"message": f"You have asked for station number: {station_id}" }
