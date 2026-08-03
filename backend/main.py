ac4e73f15b33f456e224d0f5595eea2055b31723
Omar <mohammed.omar078@gmail.com>
2026-08-03 16:36:16 -0700

    refactor: check cache dictionary for station_id validity and return cached data

--- backend/main.py
+++ backend/main.py
@@ -12,4 +12,18 @@ async def root():
 
 @app.get("/transit/{station_id}")
 async def get_transit_data(station_id: str):
-    return {"message": f"You have asked for station number: {station_id}" }
+    current_time = time.time()
+    if(station_id in cache_memory and current_time - cache_memory[station_id]["timestamp"] < 30):
+
+        # Cache hit! -> data is fresh
+        print("--- Fetching from internal memory cache ---")
+        return {
+            "source": "cache",
+            "data": cache_memory[station_id]["data"],
+        }
+    else:
+        # Cache miss (not it cache_memory or outdated ) -> request data from translink API
+        print("--- Cache empty or expired. Need to fetch from TransLink ---")
+
+        # placeholder message
+        return {"message": "Cache miss. Will fetch live data here next."}
